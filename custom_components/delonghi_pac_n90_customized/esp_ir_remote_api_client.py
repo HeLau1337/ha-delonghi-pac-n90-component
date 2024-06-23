4dae3771196b8743bca9036c205462b638c80685
Hendrik <26305045+HeLau1337@users.noreply.github.com>
2024-06-23 04:19:31 +0200

    first working version

--- custom_components/delonghi_pac_n90_customized/esp_ir_remote_api_client.py
+++ custom_components/delonghi_pac_n90_customized/esp_ir_remote_api_client.py
@@ -3,16 +3,13 @@
 import logging
 
 import aiohttp
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import requests
-from requests import Response
 
 from .const import HA_TO_DELONGHI_HVAC, HVACMode
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class EspIrRemoteApi:
+class EspIrRemoteApiClient:
     """Handles communication with the ESP8266 webserver API endpoint for setting the AC unit's state by sending the corresponding infrared signals."""
 
     DELONGHI_AC_ENDPOINT = "delonghi-ac"
@@ -22,17 +19,17 @@ class EspIrRemoteApi:
         self._base_url = base_url
         self._session = session
 
-    async def test_connection(self) -> bool:
-        """Tests the connection to the base url."""
-        async with self._session.get(f"{self._base_url}/", timeout=5) as response:
+    async def async_test_connection(self) -> bool:
+        """Test the connection to the base url."""
+        async with self._session.get(f"{self._base_url}/", timeout=600) as response:
             return response.status == 200
 
-    async def send_new_state_request(
+    async def async_set_state(
         self,
         hvac_mode: HVACMode,
         fan_mode: str,
         target_temperature: float | None = None,
-    ) -> Response:
+    ) -> aiohttp.ClientResponse:
         """Send a GET request to the API endpoint for setting a new state of the AC unit."""
         params = {}
         if hvac_mode:
@@ -42,23 +39,23 @@ class EspIrRemoteApi:
         if target_temperature:
             params["temperature"] = str(int(target_temperature))
 
-        response = requests.get(
-            f"{self._base_url}/{self.DELONGHI_AC_ENDPOINT}", params, timeout=60
-        )
-        _LOGGER.debug(
-            "Sent GET request: %s",
-            response.url,
-        )
-        if response.ok:
-            _LOGGER.debug(
-                "Response status code: %s | Response text: %s",
-                response.status_code,
-                response.text,
-            )
-        else:
-            _LOGGER.error(
-                "Response status code: %s | Response text: %s",
-                response.status_code,
-                response.text,
-            )
-        return response
+        async with self._session.get(
+            f"{self._base_url}/{self.DELONGHI_AC_ENDPOINT}", params=params, timeout=600
+        ) as response:
+            # _LOGGER.debug(
+            #    "Sent GET request: %s",
+            #    response.url,
+            # )
+            if response.status == 200:
+                _LOGGER.debug(
+                    "Response status code: %s | Response text: %s",
+                    response.status,
+                    await response.text(),
+                )
+            else:
+                _LOGGER.error(
+                    "Response status code: %s | Response text: %s",
+                    response.status,
+                    await response.text(),
+                )
+            return response
